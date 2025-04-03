6e995486f41b9a4012b226f9218994092ce9e240
RANI <rani.navale@indi4.io>
2025-04-03 11:56:55 +0530

    datetime format, holding

--- Environment/serializers.py
+++ Environment/serializers.py
@@ -366,7 +366,10 @@ class OperationsInEcologicallySensitiveAreasSerializer(serializers.ModelSerializ
 
 
 class EnvironmentalImpactAssessmentsOfProjectsUndertakenSerializer(serializers.ModelSerializer):
-    Date = serializers.DateField(format="%d-%m-%Y")
+    Date = serializers.DateField(
+        format="%d-%m-%Y",  
+        input_formats=["%Y-%m-%d", "%d-%m-%Y"]  # Allow both formats
+    )    
     class Meta:
         model = Environmental_Impact_Assessments_Of_Projects_Undertaken
         fields = "__all__"
