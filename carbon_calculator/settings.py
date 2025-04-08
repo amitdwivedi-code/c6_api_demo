99ca489547baa1bda63b71a9eee90ad475ed4c7a
@arpit.turkar <arpit.turkar@indi4.io>
2025-04-08 17:40:44 +0530

    s3 settings added

--- carbon_calculator/settings.py
+++ carbon_calculator/settings.py
@@ -21,11 +21,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 env = Env()
 env.read_env(BASE_DIR / '.env')
+
+# Media config
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = env.str('AWS_S3_ENDPOINT_URL')
+
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -64,7 +76,8 @@ INSTALLED_APPS = [
     'Workplace',
     'Reports',
     'drf_yasg',
-    'recommendation'
+    'recommendation',
+    'storages',
 ]
 
 MIDDLEWARE = [
