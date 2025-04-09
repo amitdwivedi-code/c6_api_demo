76c0341fb6284794a3390726128013d47bd81f49
@arpit.turkar <arpit.turkar@indi4.io>
2025-04-09 14:46:17 +0530

    s3 settings added in docker

--- carbon_calculator/settings.py
+++ carbon_calculator/settings.py
@@ -21,21 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 env = Env()
 env.read_env(BASE_DIR / '.env')
 
 # Media config
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_ENDPOINT_URL = env.str('AWS_S3_ENDPOINT_URL')
-
-
+AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = env.str('AWS_S3_ENDPOINT_URL')
+AWS_S3_REGION_NAME = env.str('AWS_S3_REGION_NAME')
+# DATA_DIR=env.str('DATA_DIR')
 
 
 
@@ -77,7 +77,7 @@ INSTALLED_APPS = [
     'Reports',
     'drf_yasg',
     'recommendation',
-    # 'storages',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -115,11 +115,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'carbon_calculator.wsgi.application'
 DATABASES = { 
     'default': { 
-        'ENGINE': 'djongo', 
-        'NAME': 'c6_api_demo', 
-        'CLIENT': { 
+        'ENGINE': 'djongo',
+        'NAME': 'c6_api_demo',
+        'ENFORCE_SCHEMA': False,  # Optional, helps avoid issues with schema checks
+        'CLIENT': {
             'host': env.str('MONGO_DATABASE_URL'),
-            'ssl': env.bool('SSL_KEY',True)
+            'tls': True,  # use 'tls' instead of 'ssl' for compatibility with some versions
+            'authMechanism': 'SCRAM-SHA-1',
+            'appName': 'Cluster0',
         } 
     } 
 }
---
+++ localstack/init-scripts/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Creating bucket: c6media-bucket"
+awslocal s3 mb s3://c6media-bucket
